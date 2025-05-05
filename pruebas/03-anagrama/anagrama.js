575fdd16799168e2fd3245d65a880236193a536a
Nico <nicolasaddamo1@gmail.com>
2025-05-05 18:58:27 -0300

    feat: testing commit -a

--- pruebas/03-anagrama/anagrama.js
+++ pruebas/03-anagrama/anagrama.js
@@ -2,5 +2,11 @@
 function anagrama(palabra1, palabra2) {
     let reomveSpacesAndToLowerCase = function (str) {
         let result = ''
+        for (const element of str) {
+            if (element !== ' ') {
+                result += element.toLowerCase()
+            }
+        }
+        return result
     }
 }
