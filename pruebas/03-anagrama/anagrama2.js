1977c5bf0648ecb871aa96681646ed3b154caf18
Nico <nicolasaddamo1@gmail.com>
2025-05-05 19:42:08 -0300

    added som

--- pruebas/03-anagrama/anagrama2.js
+++ pruebas/03-anagrama/anagrama2.js
@@ -21,8 +21,29 @@ function anagrama(palabra1, palabra2) {
 
 }
 
-console.log(anagrama('amor', 'roma')) // true
-console.log(anagrama('mora', 'amor')) // false
-console.log(anagrama('amor', 'ramo ')) // false
-console.log(anagrama('amor', 'rama')) // false
-console.log(anagrama('amar', 'rama')) // false
+
+const hashMap = (string) => {
+    const mappedObj = {}
+    for (const i of string) {
+        if (mappedObj[i] === undefined) mappedObj[i] = 1
+        else mappedObj[i] += 1
+    }
+    return mappedObj
+}
+const checkAnagram = (string1, string2) => {
+    if (string1.length !== string2.length) return false
+    const obj1 = hashMap(string1)
+    const obj2 = hashMap(string2)
+
+    for (const key in obj1) {
+        if (obj2[key] !== obj1[key]) return false
+    }
+    return true
+}
+
+console.log(checkAnagram('amor', 'roma')) // true
+console.log(checkAnagram('mora', 'amor')) // true
+console.log(checkAnagram('amor', 'ramo ')) // false
+console.log(checkAnagram('amor', 'rama')) // false
+console.log(checkAnagram('baba', 'aaba')) // false
+console.log(checkAnagram('aaaa', 'aaaa')) // false
